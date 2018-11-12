ab72938943763eb0042ebd5f89755b892d3ebd22
Nicolae <44190853+nicolaeonu@users.noreply.github.com>
2018-11-12 21:12:17 +0200

    Update Main.java

--- MegaBytesConverter/src/com/nicolae/Main.java
+++ MegaBytesConverter/src/com/nicolae/Main.java
@@ -6,4 +6,16 @@ public class Main {
 
         MegaBytesConverter.printMegaBytesAndKiloBytes(40000);
     }
+    
+    public static void printMegaBytesAndKiloBytes (int kiloBytes){
+        int megaByte = 1024;
+        int kiloBytesIntoMegaBytes = kiloBytes / megaByte;
+        int remainderKiloBytes = kiloBytes % megaByte;
+
+        if (kiloBytes >= 0) {
+            System.out.println(kiloBytes + " KB = " + kiloBytesIntoMegaBytes + " MB and " + remainderKiloBytes + " KB");
+        } else if(kiloBytes < 0){
+            System.out.println("Invalid Value");
+        }
+    }
 }
