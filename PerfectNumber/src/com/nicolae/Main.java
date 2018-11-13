daf2a50d473a49a2978c98ec1cbedeea9c112fa5
Nicolae <44190853+nicolaeonu@users.noreply.github.com>
2018-11-13 10:24:03 +0200

    Add files via upload

---
+++ PerfectNumber/src/com/nicolae/Main.java
@@ -0,0 +1,28 @@
+package com.nicolae;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        isPerfectNumber(6);
+    }
+
+    public static void isPerfectNumber(int number){
+        int sum = 0;
+        if(number < 1){
+            System.out.println("Invalid Number");
+        }else {
+            for (int i = 1; i < number; i++) {
+
+                if (number % i == 0) {
+                    sum += i;
+                }
+            }
+            if (sum == number) {
+                System.out.println("true");
+            }else
+                System.out.println("false");
+        }
+    }
+
+}
