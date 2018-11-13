f2c1fd7350ef38108607660b1e4d15668ecb7431
Nicolae <44190853+nicolaeonu@users.noreply.github.com>
2018-11-13 10:21:01 +0200

    Update Main.java

--- LastDigitChecker/src/com/nicolae/Main.java
+++ LastDigitChecker/src/com/nicolae/Main.java
@@ -18,7 +18,7 @@ public class Main {
             num3=num3%10;
             if((num1==num2)||(num2==num3)||(num1==num3)){
                 System.out.println("true");
-            }else
+            }else{
                 System.out.println("false");
             }
         }
