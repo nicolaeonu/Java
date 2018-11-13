67e66fddd57fda4d5338579b6f1c232d800ed19c
Nicolae <44190853+nicolaeonu@users.noreply.github.com>
2018-11-13 10:28:36 +0200

    Add files via upload

---
+++ DiagonalStar/src/com/nicolae/Main.java
@@ -0,0 +1,25 @@
+package com.nicolae;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        printSquareStar(8);
+    }
+
+    public static void printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        } else {
+
+            for (int i = 1; i <= number; i++) {
+                for (int j = 1; j <= number; j++) {
+                    if (i == 1 || i == number || j == 1 || j == number || i == j || j == number - i + 1) {
+                        System.out.print("*");
+                    } else System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
