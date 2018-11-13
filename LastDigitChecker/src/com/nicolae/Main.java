e6579502febb5370c0d8cc658be4bfe63be81049
Nicolae <44190853+nicolaeonu@users.noreply.github.com>
2018-11-13 10:20:45 +0200

    Update Main.java

--- LastDigitChecker/src/com/nicolae/Main.java
+++ LastDigitChecker/src/com/nicolae/Main.java
@@ -12,18 +12,15 @@ public class Main {
         if((num1<10)||(num1>1000)||(num2<10)||(num2>1000)||(num3<10)||(num3>1000))
         {
             System.out.println("The number is not inr the range of 10-1000");
-        }
-        else
-        {
+        }else{
             num1 %= 10;
             num2=num2%10;
             num3=num3%10;
-            if((num1==num2)||(num2==num3)||(num1==num3))
-            {
+            if((num1==num2)||(num2==num3)||(num1==num3)){
                 System.out.println("true");
-            }
-            else
+            }else
                 System.out.println("false");
+            }
         }
     }
     
