7036614469d61643eeb0433b6e5f2ddb7593a8e0
Nicolae <44190853+nicolaeonu@users.noreply.github.com>
2018-11-13 10:14:20 +0200

    Add files via upload

---
+++ LastDigitChecker/src/com/nicolae/LastDigitChecker.java
@@ -0,0 +1,25 @@
+package com.nicolae;
+
+public class LastDigitChecker {
+
+    public static void hasSameLastDigit(int num1,int num2,int num3)
+    {
+        if((num1<10)||(num1>1000)||(num2<10)||(num2>1000)||(num3<10)||(num3>1000))
+        {
+            System.out.println("The number is not inr the range of 10-1000");
+        }
+        else
+        {
+            num1 %= 10;
+            num2=num2%10;
+            num3=num3%10;
+            if((num1==num2)||(num2==num3)||(num1==num3))
+            {
+                System.out.println("true");
+            }
+            else
+                System.out.println("false");
+        }
+    }
+
+}
---
+++ LastDigitChecker/src/com/nicolae/Main.java
@@ -0,0 +1,9 @@
+package com.nicolae;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LastDigitChecker.hasSameLastDigit(11,22,33);
+    }
+}
